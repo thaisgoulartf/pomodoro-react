216569470cb5f22d1f442aa3f38770b26004254a
Thais Goulart <thaisgoulartf@gmail.com>
2025-01-12 22:22:09 -0300

    update pomodoro

--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles.ts";
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
