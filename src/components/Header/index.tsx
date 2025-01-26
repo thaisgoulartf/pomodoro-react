c2dd03b61e0864c4ebf3309a5b788a238c156d04
Thais Goulart <thaisgoulartf@gmail.com>
2025-01-25 22:04:58 -0300

    page home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export function Header() {
         <NavLink to="/" title="Timer">
           <Timer size={24} />
         </NavLink>
-        <NavLink to="/history" title="Histórico">
+        <NavLink to="/history" title="Históricoo">
           <Scroll size={24} />
         </NavLink>
       </nav>
